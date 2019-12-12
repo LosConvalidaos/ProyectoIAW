737fa4144858ff03280aa3867fffdc32331a7a8a
Nashi91 <clabiancer1@educacion.navarra.es>
2019-12-12 19:56:03 +0100

    Hijo, no me subas un rar y subeme los archivos :^)

---
+++ Nosotros/nosotros.js
@@ -0,0 +1,30 @@
+function imagen() {
+	var pic = document.getElementById('bibu')
+	pic.style.width = '100px';
+	pic.style.height = '200px';
+}	
+function imagenp() {
+	var pic = document.getElementById('bibu')
+	pic.style.width = '15%';
+	pic.style.height = '100px';
+}
+var pic2;
+function imagen2() {
+	pic2 = document.getElementById('cris')
+	pic2.style.width = '100px';
+	pic2.style.height = '200px';
+}
+function backimage() {
+	pic2.style.width= '15%';
+	pic2.style.height= '100px';
+}
+var pic3;
+function imagen3() {
+	pic3 = document.getElementById('carlos')
+	pic3.style.width = '100px';
+	pic3.style.height = '200px';
+}
+function backimage2() {
+	pic3.style.width = '15%';
+	pic3.style.height = '100px';
+}
