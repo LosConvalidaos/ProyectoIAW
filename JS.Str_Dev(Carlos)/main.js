6a6efff8223ad6d43054b5299b31941135daad1b
Nashi91 <clabiancer1@educacion.navarra.es>
2019-12-16 12:20:28 +0100

    Early OOP testing

--- JS.Str_Dev(Carlos)/main.js
+++ JS.Str_Dev(Carlos)/main.js
@@ -1,2 +1,22 @@
 /* Carlos Labiano Cerón */
 /*        2ºASIR        */
+//Porcentaje de beneficio
+var benefitTX = 0.30;
+class ShopItem {
+    constructor (Nombre,Precio,Stock,Valoracion,Imagen,Descripcion) {
+        this.name = Nombre;
+        this.cost = Precio;
+        this.qty = Stock;
+        this.rating = Valoracion;
+        this.img = Imagen;
+        this.descr = Descripcion;
+    }
+    //Devuelve el precio con IVA mas el porcentaje de beneficio
+    CalcSell() {
+        return this.cost + (this.cost*0.21)+(this.cost*benefitTX);
+    }
+}
+function pruebas() {
+    var test = new ShopItem('NombrePRB',0,0,0,'IMG','Prueba');
+    alert(test.rtn);
+}
