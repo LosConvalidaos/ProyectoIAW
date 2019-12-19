fa2643a47ecf809bde06db37a1610600f19a8e08
cris <cristianfc68@hotmail.com>
2019-12-19 09:40:49 +0100

    Add files via upload

--- Nosotros/nosotros.js
+++ Nosotros/nosotros.js
@@ -1,30 +1,36 @@
-function imagen() {
-	var pic = document.getElementById('bibu')
-	pic.style.width = '100px';
-	pic.style.height = '200px';
-}	
-function imagenp() {
-	var pic = document.getElementById('bibu')
-	pic.style.width = '15%';
-	pic.style.height = '100px';
+function imagen(i) {
+	var imagenes = new Array("bibu", "cris", "carlos");
+	var pics = new Array("pic1.png", "pic2.png", "pic3.png");
+	var titles = document.getElementById('title')
+	titles.innerHTML = imagenes[i];
+	var persons = document.getElementById('person')
+	persons.src = pics[i]
+	//imagen_txt();
+	//imagen2_txt();
+	//imagen3_txt();
+	imagen_txt_glb(i);
 }
-var pic2;
-function imagen2() {
-	pic2 = document.getElementById('cris')
-	pic2.style.width = '100px';
-	pic2.style.height = '200px';
+
+function imagen_txt_glb(i) {
+	var txts = new Array("Este es Bibu, es el director general de la empresa", "Este es Cris, es el director de la empresa", "Este es Carlos, este es el vicepresidente de la empresa");
+	var txt = document.getElementById('text');
+	txt.style.visibility = "visible";
+	txt.innerHTML = txts[i];
 }
-function backimage() {
-	pic2.style.width= '15%';
-	pic2.style.height= '100px';
+
+/*function imagen_txt() {
+	var imagenes_txt = document.getElementById('texts'),
+		textbox = imagenes_txt.getElementsByTagName('h4');
+	textbox[0].style.visibility = "visible";
+	imagen(i);
 }
-var pic3;
-function imagen3() {
-	pic3 = document.getElementById('carlos')
-	pic3.style.width = '100px';
-	pic3.style.height = '200px';
+function imagen2_txt() {
+	var imagenes2_txt = document.getElementById('texts'),
+	textbox2 = imagenes2_txt.getElementsByTagName('h4');
+	textbox2[1].style.visibility = "visible";
 }
-function backimage2() {
-	pic3.style.width = '15%';
-	pic3.style.height = '100px';
-}
+function imagen3_txt() {
+	var imagenes3_txt = document.getElementById('texts'),
+	textbox3 = imagenes3_txt.getElementsByTagName('h4');
+	textbox3[2].style.visibility = "visible";
+}*/
