61305d6a4afb5d171f16bf85fff6ec2ad0d42dcb
Nashi91 <clabiancer1@educacion.navarra.es>
2019-12-19 09:17:19 +0100

    Added dynamic product table generation

--- JS.Str_Dev(Carlos)/main.js
+++ JS.Str_Dev(Carlos)/main.js
@@ -27,7 +27,7 @@ class ShopItem {
         if (this.type == null) {
             return 'Otros';
         } else {
-            return prodtypes[this.type]
+            return prodtypes[this.type];
         }
     }
     /* Metodos */
@@ -47,24 +47,29 @@ class ShopItem {
     //Metodo que muestra de forma mas visual la valoracion
     //WIP
     ShowStars() {
-
+        
     }
 }
 //INIT
 function init() {
     alert('init');
+    // Productos globales
     prod1 = new ShopItem('Prod1',100,10,5,'Prod1.jpg','Producto 1'),
     prod2 = new ShopItem('Prod2',3,253,1,'Prod2.jpg','Producto 2'),
     prod3 = new ShopItem('Prod3',300.99,13,4.5,'Prod3.jpg','Producto 3'),
     prod4 = new ShopItem('Prod4',75,143,3,'Prod4.jpg','Producto 4'),
-    prod5 = new ShopItem('Prod5',9.99,1080,0.5,'Prod5.jpg','Producto 5');
+    prod5 = new ShopItem('Prod5',9.99,1080,0.5,'Prod5.jpg','Producto 5'),
+    prod6 = new ShopItem('Prod6',100,10,5,'Prod6.jpg','Producto 6'),
+    prod7 = new ShopItem('Prod7',3,253,1,'Prod7.jpg','Producto 7'),
+    prod8 = new ShopItem('Prod8',300.99,13,4.5,'Prod8.jpg','Producto 8'),
+    prod9 = new ShopItem('Prod9',75,143,3,'Prod9.jpg','Producto 9');
 }
 
 /* Funciones */
 function pruebas() {
     var test = new ShopItem('NombrePRB',0,0,0,'Placeholder.jpg','Prueba');
     alert(test.img);
-    alert(prod5.CalcSell())
+    alert(prod5.typestr)
 
     document.getElementById('test').src = test.img;
     /*  Pruebas de acceso a ficheros, no funciona con formato file://
@@ -72,7 +77,36 @@ function pruebas() {
     //var response = fetch('../README.md');
     //alert(response.text());
 }
+function pruebas2() {
+    var prods = [prod1,prod2,prod3,prod4,prod5,prod6,prod7,prod8,prod9],
+        randNums = rand(prods.length);
 
+    for (let i = 0; i < prods.length; i++) {
+        //Acceso estatico
+        //document.getElementsByTagName('td')[i].innerHTML = prods[i].name;
+        //Acceso aleatorio
+        document.getElementsByTagName('td')[i].innerHTML = prods[randNums[i]].name;
+    }
+}
+//HELPER | Genera numeros aleatorios para el acceso a un array dada su longitud
+function rand(ar_length) {
+    var randNums = [], //Array para los numeros a devolver
+        randN = 0; //Numero aleatorio
+    var i = 0; //Contador para rellenar el array
+
+    do {
+        //Genera un numero entre 0(Incl.) y la longitud del array(Excl.)
+        randN = Math.floor(Math.random()*(ar_length));
+        //Añade el numero si no se repite y aumenta contador
+        if (randNums.indexOf(randN) == -1) {
+            randNums[i] = randN;
+            i++;
+        }
+        //alert(randNums)
+    } while (randNums.length != ar_length);
+    //alert(randNums)
+    return randNums;
+}
 /* IDEAS */
 /*
     [Tabla de correlacion Array - Tipo prod]
