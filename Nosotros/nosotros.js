cc5593b80178efaed2d1744f4844541cb96eb9b3
Nashi91 <clabiancer1@educacion.navarra.es>
2020-01-14 13:22:52 +0100

    Idk

--- Nosotros/nosotros.js
+++ Nosotros/nosotros.js
@@ -12,25 +12,8 @@ function imagen(i) {
 }
 
 function imagen_txt_glb(i) {
-	var txts = new Array("Este es Bibu, es el director general de la empresa", "Este es Cris, es el director de la empresa", "Este es Carlos, este es el vicepresidente de la empresa");
+	var txts = new Array("Este es Bibu, es el director general de la empresa", "Este es Cris, es el presidente de la empresa", "Este es Carlos, este es el vicepresidente de la empresa");
 	var txt = document.getElementById('text');
 	txt.style.visibility = "visible";
 	txt.innerHTML = txts[i];
 }
-
-/*function imagen_txt() {
-	var imagenes_txt = document.getElementById('texts'),
-		textbox = imagenes_txt.getElementsByTagName('h4');
-	textbox[0].style.visibility = "visible";
-	imagen(i);
-}
-function imagen2_txt() {
-	var imagenes2_txt = document.getElementById('texts'),
-	textbox2 = imagenes2_txt.getElementsByTagName('h4');
-	textbox2[1].style.visibility = "visible";
-}
-function imagen3_txt() {
-	var imagenes3_txt = document.getElementById('texts'),
-	textbox3 = imagenes3_txt.getElementsByTagName('h4');
-	textbox3[2].style.visibility = "visible";
-}*/
