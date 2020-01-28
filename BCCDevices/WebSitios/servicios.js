f9e4e9775cf92e728c12bf5988d5fd6c997f10b0
Nashi91 <clabiancer1@educacion.navarra.es>
2020-01-28 21:44:23 +0100

    Finished JS and Added content

--- BCCDevices/WebSitios/servicios.js
+++ BCCDevices/WebSitios/servicios.js
@@ -5,29 +5,32 @@ function DisplayDropdown(SrvNum) {
     var ARDropArrow = document.getElementsByClassName('dropdown'),
         ARArtSrvTxt = document.getElementsByClassName('artsrvtxt');
 
-    if (ARDropArrow[SrvNum].src == 'file:///D:/Proyectos/ProyectoIAW/BCCDevices/WebSitios/sugerenciasdropup.png') {
-        ARArtSrvTxt[SrvNum].style.visibility = 'hidden';
-        document.getElementById('artsrviimg').style.visibility = 'hidden';
+    if (ARDropArrow[SrvNum].src.includes('sugerenciasdropup.png')) {
+        ARArtSrvTxt[SrvNum].style.display = 'none';
+        document.getElementById('artsrviimg').style.display = 'none';
 
-        AlterDropArrow(SrvNum)
+        AlterDropArrow(SrvNum);
     } else {
-        document.getElementById('artsrviimg').src = 'srvi' + SrvNum + '.jpg'
-        document.getElementById('artsrviimg').style.visibility = 'visible';
+        document.getElementById('artsrviimg').src = 'srvi' + SrvNum + '.jpg';
+        document.getElementById('artsrviimg').style.display = 'inline';
 
         for (let i = 0; i < ARArtSrvTxt.length; i++) {
-            ARArtSrvTxt[i].style.visibility = 'hidden'
+            ARArtSrvTxt[i].style.display = 'none';
         }
-        ARArtSrvTxt[SrvNum].style.visibility = 'visible'
+        ARArtSrvTxt[SrvNum].style.display = 'inline';
 
-        AlterDropArrow(SrvNum)
+        AlterDropArrow(SrvNum);
     }
 }
 function AlterDropArrow(ArrowSrvNum) {
     var ARDropArrow = document.getElementsByClassName('dropdown');
 
-    if (ARDropArrow[ArrowSrvNum].src == 'file:///D:/Proyectos/ProyectoIAW/BCCDevices/WebSitios/sugerenciasdropup.png') {
+    if (ARDropArrow[ArrowSrvNum].src.includes('sugerenciasdropup.png')) {
         ARDropArrow[ArrowSrvNum].src = 'sugerenciasdrop.png';
     } else {
+        for (let i = 0; i < ARDropArrow.length; i++) {
+            ARDropArrow[i].src = 'sugerenciasdrop.png';
+        }
         ARDropArrow[ArrowSrvNum].src = 'sugerenciasdropup.png';
     }
 }
