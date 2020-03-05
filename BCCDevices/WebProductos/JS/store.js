0a03407db8bae68cacd79001e8dc1b0abeeac18d
Nashi91 <clabiancer1@educacion.navarra.es>
2020-03-05 11:54:41 +0100

    Added comments and logging to console

--- BCCDevices/WebProductos/JS/store.js
+++ BCCDevices/WebProductos/JS/store.js
@@ -1,23 +1,30 @@
 /*
 Proyecto IAW JavaScript Tienda
     Carlos Labiano Cerón
-    2º ASIR | 2020-03-02
+    2º ASIR | 2020-03-05
 */
-//function prueba() {alert(1);}
+// Funcion que gestiona las redirecciones de la tienda (Productos)
 function BtnRedir(i, prod = null) {
+    //Si existe ID producto o es NULL, ERROR
     if (prod == null) {
         alert('ERROR, PRODUCTO NO EXISTENTE');
+        console.error('CRITICAL ERROR, product ID is NULL');
         return;
     }
+    //Según que boton se le llame Mas info(0) y Comprar(1), llamar a una funcion u otra
     switch (i) {
     case 0:
-        BtnRedir_More(prod);
+        BtnRedir_More(prod); //Llamar funcion de Mas Info
         break;
     case 1:
-        BtnRedir_Buy(prod);
+        BtnRedir_Buy(prod); //Llamar funcion de Compra
         break;
+    default:
+        console.warn('WARNING, button identifier can not be read or is non-existant');
     }
 }
+/* FUNCIONES AUXILIARES */
+// Funcion que muestra mas información sobre el producto (Redireccion)
 function BtnRedir_More(IDProd) {
     var id = IDProd,
         url = 'http://localhost/iaw/ProyectoIAW/BCCDevices/WebProductos/prod.php?id=';
@@ -26,18 +33,29 @@ function BtnRedir_More(IDProd) {
     
     window.location.href = urlaux + id;
 }
+// Funcion que gestiona la compra de un producto (Maqueta no funcional)
 function BtnRedir_Buy(IDProd) {
-    var id = IDProd;
-
+    var id = IDProd, //Obtiene y almacena el ID del producto seleccionado
+        datachk = true; //Almacena la correcta comprobacion de los datos
+    // Comprobar si es llamada desde la tienda (No existe cantidad) o desde el producto
     if (document.getElementById('qty') == null) {
-        console.error('ERROR, product quantity not found or is NULL/1');
-        var qty = 1;
+        console.warn('WARNING, product quantity not found or is NULL');
+        //Si no se detecta una cantidad avisar de ello
+        var qty = 1; //Establecer por defecto la cantidad como 1 unidad
     } else {
+        //Obtener la cantidad seleccionada por el usuario
         var qty = document.getElementById('qty').value;
-        if (qty == '') {qty = 1;}
+        if (qty == '') {qty = 1;} //Si la cantidad es nula, establecer por defecto a 1
+        //Filtrar entrada del usuario
+        if (parseInt(qty) <= 0 || isNaN(qty)) {
+            alert('ERROR, por favor introduzca un numero mayor que 0 como cantidad');
+            datachk = false; //Indica que no se ha superado el control de datos
+            console.error('ERROR, user data violates required format');
+        }
     }
-
-    alert('¡ENHORABUENA! Ha comprado '+qty+' unidades');
+    //Maqueta de aviso, realmente aqui se redireccionaria al carrito/sistema de pago
+    if (datachk) {alert('¡ENHORABUENA! Ha comprado '+qty+' unidades');}
+    //Si existe el campo 'Cantidad', restablecer su valor
     if (document.getElementById('qty') != null) {
         document.getElementById('qty').value = '';
     }
